57f63d67006dc36fa4a1bfcf85b80b8bce3fae02
Katsuki <73317659@continental.edu.pe>
2023-03-29 16:02:53 -0500

    Esta es la version llena de horrores en la logica

--- Registro de canciones.py
+++ Registro de canciones.py
@@ -1,6 +1,5 @@
-#esta es la primera prueba de mi programa 
-#iniciar un arreglo de informacion
 
+"""
 def InsertarCanciones(nombre,compositor,cantante, album, year):
     arreglo = []
     arreglo.append(nombre)
@@ -29,4 +28,51 @@ for i in range (0,3):
     year = print('Ingrese nombre de cancion')
     cancion = InsertarCanciones(cancion,nombre,compositor,cantante,album,year)
     lista = InsertarEnLista(cancion)---
+"""
 
+# lista para almacenar las canciones
+canciones = []
+
+# agregamos las canciones
+def agregar_cancion():
+    nombre = input("Ingrese el nombre de la canción: ")
+    compositor = input("Ingrese el nombre del compositor: ")
+    cantante = input("Ingrese el nombre del cantante: ")
+    album = input("Ingrese el nombre del álbum: ")
+    year = input("Ingrese el año de la cancion: ")
+    cancion = {
+        "nombre": nombre,
+        "compositor": compositor,
+        "cantante": cantante,
+        "album": album,
+        "year": year
+    }
+    canciones.append(cancion)
+    print("Canción agregada con éxito.")
+
+#####################################################
+
+# mostrar todas las canciones
+def mostrar_canciones():
+    if len(canciones) == 0:
+        print("No se han registrado canciones.")
+    else:
+        for cancion in canciones:
+            print(f'Cancion: {cancion["nombre"]}, Compositor: {cancion["compositor"]}, Cantante: {cancion["cantante"]}, Álbum: {cancion["album"]}')
+
+# menu para agregar y mostrar las canciones
+while True:
+    print("1. Agregar una canción")
+    print("2. Mostrar todas las canciones")
+    print("3. Salir")
+    opcion = input("Ingrese su opción: ")
+
+    if opcion == "1":
+        agregar_cancion()
+    elif opcion == "2":
+        mostrar_canciones()
+    elif opcion == "3":
+        print("Gracias por usar el programa. Hasta luego.")
+        break
+    else:
+        print("Opción inválida. Intente de nuevo.")
